6781cf89ad4eba0a4636fe5977c0863d859f2e05
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-07-25 21:03:38 +0300

    edited templates to dest and time params

--- backend/destination_params.py
+++ backend/destination_params.py
@@ -1,4 +1,5 @@
 import os
+import re
 import logging
 logger = logging.getLogger(__name__)
 
@@ -18,31 +19,42 @@ def create_destination_params(user_request):
     logger.info("Creating destination parameters...")
 
     #initialize the openai model
-    chat = ChatOpenAI(temperature=0, openai_api_key = OPENAI_API_KEY)
+    chat = ChatOpenAI(temperature=0.5, openai_api_key = OPENAI_API_KEY)
 
     #create the prompt templates
-    system_template = """/
+    system_template = """INSTRUCTIONS:
+    You're an intelligent AI agent, and your job is to identify as many possible destination airports as you can based on the user's message. You will first think about the task, and then provide an exhaustive list of IATA airport codes that match the criteria. Always present these codes in a list format like [XXX,YYY,ZZZ]. In most cases, aim for at least 15 to 20 destinations. Including more options will increase the likelihood of finding the best flights for the user.
+
+    EXAMPLES:
     User origin: Stockholm
-    User message: I want to travel somewhere in southern Europe that is by Mediterranean.
-    Possible destinations (IATA codes): [BCN,VLC,MRS,NCE,FCO,NAP,ATH,SKG,SPU,SBV]
-    ---
+    User message: I want to travel somewhere in southern Europe that is by the Mediterranean.
+    Thought: The user wants to travel to southern Europe, specifically a location by the Mediterranean Sea. This includes countries like Spain, France, Italy, Malta, Slovenia, Croatia, Bosnia and Herzegovina, Montenegro, Albania, Greece, and Cyprus. I have included as many relevant airports as possible that have direct flights from Stockholm and are located near the Mediterranean Sea.
+    Destinations (IATA codes): [BCN,VLC,MRS,NCE,FCO,NAP,ATH,SKG,SPU,DBV,PMO,BOD,TLS,AHO,CAG,CTA,LCA,PFO,TIA,OLB,MLA,GRO]
+
     User origin: Barcelona
     User message: I want to go for a weekend getaway to a city in eastern Europe. I’d like to see some to a slightly less big city, so not just Budapest or Bucharest etc.
-    Possible destinations (IATA codes): [LWO,KIV,CLJ,GDN,BRQ,TSR,VAR,TAY,RJK,KSC]
-    ---
+    Thought: The user is interested in traveling to a less populated city in eastern Europe, excluding larger cities like Budapest or Bucharest. Thus, I've included an extensive list of smaller airports in Eastern Europe.
+    Destinations (IATA codes): [LWO,KIV,CLJ,GDN,BRQ,TSR,VAR,TAY,RJK,KSC,ODE,POZ,IEV,LVIV,SZZ,SOJ,VNO,KRK,SKP,TGD,SJJ,PRN,BEG]
+
     User origin: Munich
     User message: I need to get absolutely hammered in some cool nightclub.
-    Possible destinations (IATA codes): [IBZ,BCN,AMS,PRG,BUD,LIS,DUB,SPU,KRK,CDG]
-    ---
+    Thought: The user is looking for a city renowned for its nightlife. Cities known for their nightclubs and party scenes are numerous. So, I've included a wide range of potential locations.
+    Destinations (IATA codes): [IBZ,BCN,AMS,PRG,BUD,LIS,DUB,SPU,KRK,CDG,BER,LON,CPH,ROM,MAD,RIX,TLL,HEL,OSL,SOF,ZAG,BEG]
+
     User origin: Paris
     User message: I want to go to Amsterdam for a week in the summer.
-    Possible destinations (IATA codes): [AMS]
-    ---
-    User origin: Helsinki
+    Thought: The user has a specific destination in mind: Amsterdam. Therefore, the only relevant destination airport code is that of Amsterdam.
+    Destinations (IATA codes): [AMS]
+
+    User origin: Sydney
+    User message: I just want to get out of here, I don't care where.
+    Thought: The user is looking to travel, but hasn't specified a particular destination. Therefore, I've considered popular and accessible destinations from Sydney. The list includes a diverse range of domestic and international locations to offer the user an extensive array of choices.
+    Possible destinations (IATA codes): [MEL,BNE,ADL,PER,CBR,OOL,AKL,CHC,WLG,ZQN,NAN,DPS,SIN,KUL,BKK,HKT,HKG,TPE,NRT,HND,ICN,PEK,PVG,SFO,LAX,YVR,HNL,JFK,LHR,DXB,DOH]
     """
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-    human_template = """User message: {user_request}
-    Possible destinations (IATA codes): """
+    human_template = """User origin: Helsinki
+    User message: {user_request}
+    Thought: """
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     chat_prompt = ChatPromptTemplate.from_messages(
         [system_message_prompt, human_message_prompt]
@@ -55,16 +67,24 @@ def create_destination_params(user_request):
         ).to_messages()
     )
 
-    # Remove the brackets and spaces, then split the string into a list on the commas
-    destination_list = openai_response.content.replace("[", "").replace("]", "").replace(" ", "").split(",")
+    # Regular expression pattern to match the IATA codes
+    pattern = r'\[([A-Za-z,\s]+)\]'
 
-    # Now you should be able to join the list into a single string with commas
-    destination_string = ",".join(destination_list)
+    # Find the matches in the response content
+    matches = re.search(pattern, openai_response.content)
 
-    # Create a destination dictionary from the response
-    destination_params = {
-        'fly_to' : destination_string,
-    }
+    # If a match was found
+    if matches:
+        # Get the matched string, remove spaces, and split it into a list on the commas
+        destination_list = matches.group(1).replace(" ", "").split(',')
+
+        # Create a destination dictionary from the response
+        destination_params = {
+            'fly_to' : ','.join(destination_list),
+        }
+
+    else:
+        destination_params = {}
 
     logger.info("Destination parameters created: %s", destination_params)
     return destination_params
--- backend/time_params.py
+++ backend/time_params.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import logging
 logger = logging.getLogger(__name__)
 
@@ -16,88 +17,35 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 def create_time_params(user_request):
     logger.info("Creating time parameters...")
+    current_date_unformatted = datetime.now()
+    current_date = f"{current_date_unformatted:%d/%m/%Y}"
 
     #initialize the openai model
-    chat = ChatOpenAI(temperature=0, openai_api_key = OPENAI_API_KEY)
+    chat = ChatOpenAI(temperature=0.5, openai_api_key = OPENAI_API_KEY)
 
     #create the prompt templates
-    system_template = """/
-    ANSWER INSTRUCTIONS:
-    The output should be a markdown code snippet formatted in the following schema, including the leading and trailing "\`\`\`json" and "\`\`\`":
+    system_template = """API DOCUMENTATION:
+    date_from, date_to: Range for outbound flight departure (dd/mm/yyyy). 
 
+    nights_in_dst_from, nights_in_dst_to: Minimum and maximum stay length at the destination (in nights). Only exclude these if the user is looking for a one-way trip. Otherwise you must make an assumption.
+
+    fly_days, ret_fly_days: List of preferred days for outbound and return flights (0=Sunday, 1=Monday, ... 6=Saturday). 
+
+    fly_days_type, ret_fly_days_type: Specifies if fly_days/ret_fly_days is for an arrival or a departure flight. 
+
+    dtime_from, dtime_to, ret_dtime_from, ret_dtime_to: Define the earliest and latest departure time for outbound and return flights. 
+
+    atime_from, atime_to, ret_atime_from, ret_atime_to: Define the earliest and latest arrival time for outbound and return flights.
+
+
+    EXAMPLES:
+    Current date: 10/07/2023
+    User: "Weekend getaway in Paris next month. Off work on Friday around 3pm."
+    Answer: User wants to leave on a Friday next month and stay for two nights. Outbound flight should be after work (assumed 5PM), return flight should not be too late for work next day.
     ```json
     {{
-        "key1": value1  // Define relevant values  
-        "key2": value2 
-    }}
-    ``` 
-
-    API DOCUMENTATION:
-    date_from *
-    departure date (dd/mm/yyyy). Use parameters date_from and date_to to define the range for the outbound flight departure.
-    For example, parameters 'date_from=01/04/2021' and 'date_to=03/04/2021' mean that the departure can be anytime between the specified dates, i.e. on 01/04, 02/04 or 03/04.
-    For the dates of the return flights, use the 'nights_in_dst_from' and 'nights_in_dst_to' parameters.
-    Example : 01/04/2021
-    01/04/2021
-
-    date_to *
-    departure date (dd/mm/yyyy). Use parameters date_from and date_to to define the range for the outbound flight departure.
-    Example : 03/04/2021
-    nights_in_dst_from
-    integer
-    the minimal length of stay in the destination given in the fly_to parameter.
-    Example : 2
-
-    nights_in_dst_to
-    integer
-    the maximal length of stay in the destination given in the fly_to parameter.
-
-    Either both parameters 'nights_in_dst_to' and 'nights_in_dst_from' have to be specified or none of them.
-    Example : 3
-
-    fly_days
-    the list of week days for the flight, where 0 is Sunday, 1 is Monday, etc.
-    You can include more days than one, e.g. '&fly_days=0&fly_days=1&fly_days=2&...&fly_days=6'
-
-    fly_days_type
-    used to specify whether the fly_days day is for an arrival or a departure flight.
-    Available values : departure, arrival
-
-    ret_fly_days
-    the list of week days for the flight, where 0 is Sunday, 1 is Monday, etc.
-    URL encoded format for all days: '&ret_fly_days=0&ret_fly_days=1&ret_fly_days=2&...&ret_fly_days=6'
-
-    ret_fly_days_type
-    type of set ret_fly_days; It  is used to specify whether the flight is an arrival or a departure.
-    Available values : departure, arrival
-
-    For time related parameters below, only use time in whole hours, not minutes; 11:00 means 11AM, 23:00 means 11PM).
-
-    dtime_from
-    result filter, min. departure time.
-    dtime_to
-    result filter, max departure time.
-    atime_from
-    result filter, min arrival time.
-    atime_to
-    result filter, max arrival time.
-    ret_dtime_from
-    result filter, min dep. time of the returning flight.
-    ret_dtime_to
-    result filter, max dep. time of the returning flight.
-    ret_atime_from
-    result filter, min arrival time of the returning flight.
-    ret_atime_to
-    result filter, min arrival time of the returning flight.
-
-    USER INTERACTIONS:
-    User request: I want a weekend getaway in Paris in September. I get off work on Friday around 3pm.
-    Thought: Flight dates to/from should be set to September. Because it is a weekend getaway, the user probably wants a departure on Friday and want to stay for two nights. Also, because they get off work at 3pm, they probably can’t make it to a flight departing before 5pm. Since the user will be spending just the weekend, they don’t want a flight that departures very late on Friday or very early on Sunday. Also, as they probably have work on Monday, having a departure very late on Sunday would likely be annoying.
-    Flight time query:
-    ```json
-    {{
-        'date_from': '01/09/2023',
-        'date_to': '30/09/2023',
+        'date_from': '01/08/2023',
+        'date_to': '31/08/2023',
         'fly_days': 5,
         'fly_days_type': 'departure',
         'dtime_from': '17:00',
@@ -110,23 +58,25 @@ def create_time_params(user_request):
         'ret_dtime_to': '18:00',
     }}
     ```
-    User request: I’m on vacation from 14th of March to 18th of April. I want to go abroad for about a week.
-    Thought: The “date_from” variable should be set from 14th of March to 10th of April, so that the whole trip can be done within the timeframe. The user wants the trip to last about a week, so nights in destination should be from 6 to 8 days. As the user is on vacation, it probably doesn’t matter on which days of the week they are flying or at what time.
-    Flight time query:
+
+    Current date: 01/01/2024
+    User: "On vacation next March. Want to go abroad for about a week."
+    Answer: The trip can be done within the vacation dates next March, lasting about a week.
     ```json
     {{
-        'date_from': '14/03/2023',
-        'date_to': '10/04/2023',
-        'nights_in_dst_from': 6,
-        'nights_in_dst_to': 8,
+    'date_from': '01/03/2024',
+    'date_to': '31/03/2024',
+    'nights_in_dst_from': 6,
+    'nights_in_dst_to': 8,
     }}
     ```
-    User request: I want to go for a long weekend trip in October. I can get either Friday or Monday off from work.
-    Thought: A long weekend trip usually consists of three days. Given the user can take either Friday or Monday off, the possible travel days would be Thursday, Friday, Saturday, Sunday or Monday. Since the departure date is in October, the "date_from" and "date_to" parameters should cover the whole month. The "nights_in_dst_from" and "nights_in_dst_to" parameters should both be set to 3 to match the desired length of stay. The user didn't provide specific information about their work schedule or preferred flight times, we don't have clear constraints for the departure and arrival times. However, we can make reasonable assumptions that would generally suit most working professionals.
-    Assuming that the user works a typical 9-5 job, they might prefer to leave in the evening on their departure day to avoid taking additional time off work. We could set the "dtime_from" to be after their work, say 18:00 (6PM), and leave "dtime_to" open to accommodate late flights.
-    For the return, the user needs to be back for work on Tuesday, so a late return on Monday may be inconvenient. We might set the "ret_dtime_to" as 20:00 (8PM) to ensure they're not getting back too late.
-    Flight time query:
+
+    Current date: 10/08/2023
+    User: "Long weekend trip next October. Can get either Friday or Monday off from work."
+    Answer: Long weekend usually means three days. Possible travel days are Thursday, Friday, Saturday, Sunday, or Monday. Outbound flight should be after work (assumed 6PM), return flight not too late for work next day.
     ```json
+
+
     {{
         'date_from': '01/10/2023',
         'date_to': '31/10/2023',
@@ -137,10 +87,46 @@ def create_time_params(user_request):
         'ret_fly_days': [0, 1],
         'ret_dtime_to': '20:00',
     }}
-    ```"""
+    ```
+
+    Current date: 10/04/2023
+    User: "One-way trip to Paris in the summer."
+    Answer: The user only needs an outbound flight to Paris, which should be anytime in the summer months (June, July, August).
+    ```json
+    {{
+        'date_from': '01/06/2023',
+        'date_to': '31/08/2023',
+    }}
+    ```
+
+    Current date: 10/07/2023
+    User: "Want to go abroad."
+    Answer: The user is very vague about when they want to go or for how long. Assume about one-week stay and looking for flights in the next three months.
+    ```json
+    {{
+        'date_from': '11/07/2023',
+        'date_to': '10/10/2023',
+        'nights_in_dst_from': 5,
+        'nights_in_dst_to': 9,
+    }}
+    ```
+
+    ANSWER INSTRUCTIONS:
+    The output should include both:
+    1) Thought: Thinking out loud about the user's needs and the task.
+    2) Markdown code snippet formatted in the following schema, including the leading and trailing "\`\`\`json" and "\`\`\`":
+
+    ```json
+    {{
+        "key1": value1  // Define relevant values  
+        "key2": value2 
+    }}
+    ``` """
+
     system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-    human_template = """User request: {user_request}
-    Thought: """
+    human_template = """Current date: {current_date}
+    User: {user_request}
+    Answer: """
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     chat_prompt = ChatPromptTemplate.from_messages(
         [system_message_prompt, human_message_prompt]
@@ -149,7 +135,8 @@ def create_time_params(user_request):
     #request the response from the model
     openai_response = chat(
         chat_prompt.format_prompt(
-            user_request=user_request
+            user_request=user_request,
+            current_date=current_date
         ).to_messages()
     )
     logger.info(str(openai_response.content))

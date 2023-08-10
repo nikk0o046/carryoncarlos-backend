b98b2f5a780bf90a6ed5c709876c3f889a2ba7ba
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-08-10 18:41:07 +0300

    dockerize chat.py

--- flights_chat/chat.py
+++ flights_chat/chat.py
@@ -112,5 +112,5 @@ def handle_conversation(conversation_history):
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8081))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
